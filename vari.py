9c6903bdf1d263f06a98cdfb948f6a1b094286ab
coreyabrown <36800324+coreyabrown@users.noreply.github.com>
2018-02-28 19:34:55 -0600

    Checking for blank prediction dictionary
    Blank prediction dictionaries were causing some odd issues after JS changes. Fixed.

--- vari.py
+++ vari.py
@@ -1,4 +1,5 @@
 import tbapy
+import gviz_api
 
 
 def setapikey():
@@ -7,8 +8,8 @@ def setapikey():
     return apikey
 
 
-def frc(points, matchCount):
-    output = round(points / matchCount, 2)
+def frc(points, matchcount):
+    output = round(points / matchcount, 2)
     return output
 
 
@@ -16,6 +17,17 @@ def createlink(matchkeystring, linktext):
     link = "<a href=https://www.thebluealliance.com/match/" + matchkeystring + " target='_blank'>" + linktext +\
            "</a>"
     return link
+
+
+def predicttable(desc, dictionary):
+    data_table_predict = gviz_api.DataTable(desc)
+    data_table_predict.LoadData(dictionary)
+
+    # Data set for predictions
+    jspredict = data_table_predict.ToJSCode("jspredict_data",
+                                            columns_order=("Match Key", "Winner", "Blue 1", "Blue 2", "Blue 3",
+                                                           "Blue Score", "Red Score", "Red 1", "Red 2", "Red 3"))
+    return jspredict
 
 
 tba = tbapy.TBA(setapikey())
@@ -33,3 +45,4 @@ htmlFilePath = (event + "\\" + htmlFile)
 teamObjects = tba.event_teams(event, simple="true")
 dictList = {}
 dictChart = {}
+dictPredictEmpty = {"No Match to Predict": ["None", "tie", 0, 0, 0, 0, 0, 0, 0, 0]}
